8f32ff2d67831d9edb8b48ebd996c1ddf1fdc462
kashmiry <4423561+kashmiry@users.noreply.github.com>
2025-11-11 19:04:08 +0300

    Refactor favorites name initialization to delay translation loading until init

--- app/Favorites.php
+++ app/Favorites.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 /**
 * Static Wrapper for Bootstrap Class
 * Prevents T_STRING error when checking for 5.3.2
 */
-class Favorites 
+class Favorites
 {
 	public static function init()
 	{
@@ -15,7 +15,12 @@ class Favorites
 		$favorites_version = '2.3.6';
 
 		global $favorites_name;
-		$favorites_name = __('Favorites', 'favorites');
+		$favorites_name = 'Favorites';
+		// Delay translation loading until init to avoid _load_textdomain_just_in_time notice.
+		add_action( 'init', function() {
+			global $favorites_name;
+			$favorites_name = __('Favorites', 'favorites');
+		}, 5 );
 
 		$app = new Favorites\Bootstrap;
 	}
