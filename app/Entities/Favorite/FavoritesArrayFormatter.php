6aac7ee314b4d972cd997c4570e3163e57c6488d
kashmiry <4423561+kashmiry@users.noreply.github.com>
2025-11-11 19:05:12 +0300

    Enhance resetIndexes method to support legacy scalar entries while preserving arrays from newer formats

--- app/Entities/Favorite/FavoritesArrayFormatter.php
+++ app/Entities/Favorite/FavoritesArrayFormatter.php
@@ -62,18 +62,29 @@ class FavoritesArrayFormatter
 	*/
 	private function resetIndexes()
 	{
-		foreach ( $this->formatted_favorites as $site => $site_favorites ){
-			// Make older posts compatible with new name
-			if ( !isset($site_favorites['posts']) ) {
-				$site_favorites['posts'] = $site_favorites['site_favorites'];
-				unset($this->formatted_favorites[$site]['site_favorites']);
-			}
-			foreach ( $site_favorites['posts'] as $key => $favorite ){
-				unset($this->formatted_favorites[$site]['posts'][$key]);
-				$this->formatted_favorites[$site]['posts'][$favorite]['post_id'] = $favorite;
+			foreach ( $this->formatted_favorites as $site => $site_favorites ){
+				// Make older posts compatible with new name
+				if ( !isset($site_favorites['posts']) ) {
+					$site_favorites['posts'] = $site_favorites['site_favorites'];
+					unset($this->formatted_favorites[$site]['site_favorites']);
+				}
+				foreach ( $site_favorites['posts'] as $key => $favorite ){
+					unset($this->formatted_favorites[$site]['posts'][$key]);
+					// Support legacy scalar entries while preserving arrays coming from newer formats.
+					if ( is_array($favorite) ) {
+						$post_id = isset($favorite['post_id']) ? $favorite['post_id'] : $key;
+						$this->formatted_favorites[$site]['posts'][$post_id] = $favorite;
+						if ( !isset($this->formatted_favorites[$site]['posts'][$post_id]['post_id']) ) {
+							$this->formatted_favorites[$site]['posts'][$post_id]['post_id'] = $post_id;
+						}
+						continue;
+					}
+					$this->formatted_favorites[$site]['posts'][$favorite] = [
+						'post_id' => $favorite,
+					];
+				}
 			}
 		}
-	}
 
 	/**
 	* Add the post type to each favorite
